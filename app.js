f20a3e9665bc775e04a53ccdb6576f20404e6ec8
Filip Fasko <fasko.filip26@gmail.com>
2019-11-12 17:59:58 +0100

    Added HTML file

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var express = require("express");
 var app = express();
 var appID = "26e379c13e6541d7ad63d493fceab562";
+const bodyParser = require('body-parser');
+//var ejs = require("ejs");
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', function(req, res) {
-    res.render('index.ejs',{message:'errorMessage'});
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000, function() {
     console.log("Server is running")
+});
+
+app.post('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
 
 let url = 'https://openexchangerates.org/api/latest.json?app_id=26e379c13e6541d7ad63d493fceab562';
