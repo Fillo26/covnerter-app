77b883f230ad883b2e9aa502167c8ea27b7af1f3
Filip Fasko <fasko.filip26@gmail.com>
2019-11-12 14:35:42 +0100

    Added API request with main body response.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Currency Converting Application
 -------------------------------
 
-This is a simple application, for currency converting, including frontend and simple API.k
+This is a simple application, for currency converting, including frontend and simple API.
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var appID = "26e379c13e6541d7ad63d493fceab562";
 
 app.get('/', function(req, res) {
     res.send("Hello from app.js!")
@@ -7,4 +8,15 @@ app.get('/', function(req, res) {
 
 app.listen(3000, function() {
     console.log("Server is running")
+});
+
+let url = 'https://openexchangerates.org/api/latest.json?app_id=26e379c13e6541d7ad63d493fceab562';
+var request = require('request');
+
+request(url, function (err, response, body) {
+    if(err){
+        console.log('error:', error);
+    } else {
+        console.log(body);
+    }
 });
