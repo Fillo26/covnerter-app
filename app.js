7951793f01b19e60bb85e3702f063f7c75ccd25d
Filip Fasko <fasko.filip26@gmail.com>
2019-11-14 21:29:46 +0100

    Added jQuery script for sending data to server & implemented conversion calculating function

--- app.js
+++ app.js
@@ -1,55 +1,59 @@
 var express = require("express");
 var app = express();
-var appID = "26e379c13e6541d7ad63d493fceab562";
 const bodyParser = require('body-parser');
-var jsonData, currencies;
+var currencies, conRates;
 app.use(express.static('public'));
-var path = require('path');
 app.use('/public', express.static(__dirname + '/public'));
-//app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//********************************send HTML site to the user************************************
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-//request data from API, save as JSON to currencies
+//*****************request currency data from API, save as JSON to currencies**********************
 let urlCurrencies = 'https://openexchangerates.org/api/currencies.json';
-var request = require('request');
-request(urlCurrencies, function(err, response, body){
+let requestCur = require('request');
+requestCur(urlCurrencies, function(err, response, body){
     if(err){
         console.log('error:', error);
     } else {
         currencies = JSON.parse(body);
     }
 });
-//send received data to client side
-app.get('/currencies', function(req, res){
-    res.send(currencies);
-})
 
-app.listen(3000, function() {
-    console.log("Server is running")
-});
-
-app.post('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
+//*****************request currency rates data from API, save as JSON to conRates*****************
 let url = 'https://openexchangerates.org/api/latest.json?app_id=26e379c13e6541d7ad63d493fceab562';
-/*var request = require('request');
-
-
+let request = require('request');
 request(url, function (err, response, body) {
     if(err){
         console.log('error:', error);
     } else {
-        let conRates = JSON.parse(body);
-        jsonData = conRates;
-        console.log(body);
-        console.log("1 USD = " + conRates.rates.EUR + " EUR.");
-        var convertToUSD = 1/conRates.rates.CZK;
-        var convertToFinal = convertToUSD*conRates.rates.GBP;
-        console.log("1 CZK = " +  convertToFinal + " GBP.");
+        conRates = JSON.parse(body);
     }
-});*/
+});
+
+//**********************************send received data to client side****************************
+app.get('/currencies', function(req, res){
+    res.send(currencies);
+});
+
+//***************************************launch the server***************************************
+app.listen(3000, function() {
+    console.log("Server is running")
+});
+
+//*****************Receive data from client, calculate conversion ,send result back to client*******
+app.get('/convert/:amount-:from-:to', function(req, res){
+    const amount = req.params.amount;
+    const from = req.params.from;
+    const to = req.params.to;
+    console.log(amount,from,to);
+    if(from === "USD"){
+        result = amount * conRates.rates[to];
+    }else{
+        let midResult = amount / conRates.rates[from];
+        result = midResult * conRates.rates[to];
+    }
+    res.json(result);
+});
